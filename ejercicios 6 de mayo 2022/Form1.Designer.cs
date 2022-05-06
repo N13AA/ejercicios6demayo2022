c62edba9e1cd7e33ad1696fe26b2c4a912586c97
N13AA <nestorjosuearana68@gmail.com>
2022-05-06 13:28:43 -0500

    act

--- ejercicios 6 de mayo 2022/Form1.Designer.cs
+++ ejercicios 6 de mayo 2022/Form1.Designer.cs
@@ -28,13 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(329, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Eliga el sistema:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Menu de promedios";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(363, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Meno de calculos de bits";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(648, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 43);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Menu para calcular el numero mayor de 3 cifras";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- ejercicios 6 de mayo 2022/Form1.cs
+++ ejercicios 6 de mayo 2022/Form1.cs
@@ -16,5 +16,31 @@ namespace ejercicios_6_de_mayo_2022
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 prom = new Form2();
+            prom.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 bits = new Form3();
+            bits.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form4 nummayor = new Form4();
+            nummayor.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
